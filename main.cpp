8932df5d8963d1e534b576ac7c574c2766adce87
Arjun G. Menon <contact@arjungmenon.com>
2016-04-21 17:23:57 -0400

    Read and print test CSV file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
+
 #include <iostream>
+#include <fstream>
+#include <stdexcept>
 
 using namespace std;
 
+
+void load_data_from_file(string filename)
+{
+    ifstream csv_file(filename);
+
+    if(csv_file.fail()) {
+        throw runtime_error(string("File \"") + filename + "\" does not exist.");
+    }
+
+    string line;
+    while(getline(csv_file, line)) {
+        cout << line << endl;
+    }
+}
+
 int main() {
-    cout << "Hello, World!" << endl;
+    load_data_from_file("test_data/t1.txt");
+
     return 0;
 }

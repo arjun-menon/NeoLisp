2e6569e547e949cb298c24f6724cf6fd33b4cba0
Arjun G. Menon <contact@arjungmenon.com>
2016-12-03 02:46:46 -0500

    Move main() contents to stuff()

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include "stdinclude.h"
 
-int main() {
+void stuff() {
     Table table;
     DataReader::read_file("test_data/t1.txt", table);
     cout << "table" << endl << table << endl;
     cout << table[3].getMin() << endl;
     table.display(cout, {1, 3});
     cout << endl << *(table[1] + table[3]) << endl;
+}
+
+int main() {
+    stuff();
     return 0;
 }

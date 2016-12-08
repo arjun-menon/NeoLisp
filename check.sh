643ba97c19e4578b82b87e0c190393959f1f6f3d
Arjun G. Menon <contact@arjungmenon.com>
2016-12-08 06:04:30 -0500

    Fix check.sh (valgrind)

--- check.sh
+++ check.sh
@@ -4,4 +4,4 @@
 cppcheck -j 8 --language=c++ --std=c++11 --enable=warning,performance,unusedFunction,style src
 
 # Run valgrind
-valgrind --leak-check=full --track-origins=yes --suppressions=tools/.mac_leak.supp ./vcalc_unit_tests
+valgrind --leak-check=full --track-origins=yes --suppressions=tools/.mac_leak.supp ./cbuild/vcalc_unit_tests

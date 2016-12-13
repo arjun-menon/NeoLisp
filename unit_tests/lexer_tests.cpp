01a82bee0138da3d81356f35ad3fbc328d1ed093
Arjun G. Menon <contact@arjungmenon.com>
2016-12-13 03:19:58 -0500

    Add more Lexer unit tests

--- unit_tests/lexer_tests.cpp
+++ unit_tests/lexer_tests.cpp
@@ -1,11 +1,13 @@
 #include <catch.hpp>
 #include "../src/common.hpp"
 
-#define T(TYPE) typeid(TYPE).hash_code()
-
 bool checkLexerOutput(string input, const vector< pair<size_t, string> > &expectedOutput) {
     Lexer lexer;
     lexer.lex(input);
+
+    cout<<"Testing Lexer Output: ";
+    lexer.display(cout, false);
+    cout<<endl;
 
     size_t i = 0;
     for(auto &token : lexer.tokens) {
@@ -21,10 +23,43 @@ bool checkLexerOutput(string input, const vector< pair<size_t, string> > &expect
     return true;
 }
 
+#define T(TYPE) typeid(TYPE).hash_code()
+
 TEST_CASE("Basic expressions") {
-    CHECK(checkLexerOutput("1+2", {
-            { typeid(Real).hash_code(), "1" },
-            { typeid(Symbol).hash_code(), "+" },
-            { typeid(Real).hash_code(), "2" }
-    }));
+    CHECK(checkLexerOutput(
+            "1+2",
+            {
+                    { T(Real), "1" },
+                    { T(Symbol), "+" },
+                    { T(Real), "2" }
+            }
+    ));
+
+    CHECK(checkLexerOutput(
+            "1 (2*4) +5",
+            {
+                    { T(Real), "1" },
+                    { T(ExprStart), "(" },
+                    { T(Real), "2" },
+                    { T(Symbol), "*" },
+                    { T(Real), "4" },
+                    { T(ExprEnd), ")" },
+                    { T(Symbol), "+" },
+                    { T(Real), "5" }
+            }
+    ));
+
+    CHECK(checkLexerOutput(
+            " 2  7 asdk ( + ) 34.212aa 909", // todo: '34.212aa' should be two tokens: '34.212' and 'aa'
+            {
+                    { T(Real), "2" },
+                    { T(Real), "7" },
+                    { T(Symbol), "asdk" },
+                    { T(ExprStart), "(" },
+                    { T(Symbol), "+" },
+                    { T(ExprEnd), ")" },
+                    { T(Real), "34.212" },
+                    { T(Real), "909" }
+            }
+    ));
 }

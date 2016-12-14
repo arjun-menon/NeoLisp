f4ecd92704526bfc9a4f12f9e04244b94635d42d
Arjun G. Menon <contact@arjungmenon.com>
2016-12-14 06:44:18 -0500

    Provide - and * builtin arithmetic functions

--- src/builtin.cpp
+++ src/builtin.cpp
@@ -13,7 +13,7 @@ public:
     }
 };
 
-void define_builtin_functions(Env &env) {
+static void define_arithmetic_function(Env &env) {
     env.insert(make_pair(
             "+",
             unique_ptr<Value>(new BuiltinFunction(
@@ -30,4 +30,49 @@ void define_builtin_functions(Env &env) {
                     })
             )
     ));
+
+    env.insert(make_pair(
+            "-",
+            unique_ptr<Value>(new BuiltinFunction(
+                    "sub",
+                    [](unique_ptr<List> args, unsigned short pivot) -> unique_ptr<Value> {
+                        Real left_sum(0.0f);
+                        Real right_sum(0.0f);
+
+                        for(unique_ptr<Value> &x : args->lst) {
+                            if (isType<Real>(*x)) {
+                                Real& real = dynamic_cast<Real&>(*x);
+
+                                if(pivot--)
+                                    left_sum = left_sum + real;
+                                else
+                                    right_sum = right_sum + real;
+                            }
+                        }
+
+                        return unique_ptr<Real>(new Real(left_sum -  right_sum));
+                    })
+            )
+    ));
+
+    env.insert(make_pair(
+            "*",
+            unique_ptr<Value>(new BuiltinFunction(
+                    "div",
+                    [](unique_ptr<List> args, unsigned short pivot) -> unique_ptr<Value> {
+                        unique_ptr<Real> product(new Real(1.0f));
+                        for(unique_ptr<Value> &x : args->lst) {
+                            if (isType<Real>(*x)) {
+                                Real& real = dynamic_cast<Real&>(*x);
+                                *product = *product * real;
+                            }
+                        }
+                        return product;
+                    })
+            )
+    ));
+}
+
+void define_builtin_functions(Env &env) {
+    define_arithmetic_function(env);
 }

460f7cd7f0278309ee5d42d0767d80cde6aea66a
Arjun G. Menon <contact@arjungmenon.com>
2016-12-14 05:14:26 -0500

    In eval, for List types, evaluate all elements

--- src/eval.cpp
+++ src/eval.cpp
@@ -2,7 +2,16 @@
 
 unique_ptr<Value> eval(unique_ptr<Value> v, Env& env) {
     if (isType<List>(*v)) {
-        return v;
+        /*
+         * Evaluate all of the elements in the list
+         */
+        List* list = dynamic_cast<List*>(v.get());
+        unique_ptr<List> evaluatedList(new List);
+
+        for(auto i =  list->lst.begin(); i != list->lst.end(); i++)
+            evaluatedList->lst.emplace_back(eval(move(*i), env));
+
+        return evaluatedList;
     }
     else if (isType<Symbol>(*v)) {
         //return env[dynamic_cast<Symbol *>(v.get())->sym];

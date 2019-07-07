121b484609aa741cbf8460dc84bc98e9036784de
Arjun G. Menon <arjungmenon@gmail.com>
2019-07-08 00:06:11 +0400

    [minor] language_tests.cpp: remove unnecesary parantheses

--- unit_tests/language_tests.cpp
+++ unit_tests/language_tests.cpp
@@ -72,6 +72,6 @@ TEST_CASE("Eval simple lambda expressions") {
 }
 
 TEST_CASE("Test Map") {
-    CHECK_THAT(eval("(map (a 1) (b 2))"), Equals("({ 'a': 1, 'b': 2 })"));
-    CHECK_THAT(eval("(map (a (+ 3 4)) (b (* 3 4)))"), Equals("({ 'a': 7, 'b': 12 })"));
+    CHECK_THAT(eval("map (a 1) (b 2)"), Equals("{ 'a': 1, 'b': 2 }"));
+    CHECK_THAT(eval("map (a (+ 3 4)) (b (* 3 4))"), Equals("{ 'a': 7, 'b': 12 }"));
 }

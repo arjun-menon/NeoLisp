218f191b91ebf36d87b4c5d1430367e19237149d
Arjun G. Menon <arjungmenon@gmail.com>
2019-07-07 19:22:55 +0400

    Add two more similar factorial unit tests

--- unit_tests/language_tests.cpp
+++ unit_tests/language_tests.cpp
@@ -64,6 +64,8 @@ TEST_CASE("Eval simple lambda expressions") {
         factorial = (fn (n) (? n (* n (factorial (n - 1))) 1));
         factorial 8;
     )code"), Equals("40320"));
+    CHECK_THAT(eval("factorial = (fn (x) (? x (* x (factorial (x - 1))) 1)); factorial 10"), Equals("3.6288e+06"));
+    CHECK_THAT(eval("factorial = (fn (x) (? x (* x (factorial (x - 1))) 1)); factorial 100"), Equals("9.33262e+157"));
 
     CHECK_THROWS_WITH(eval("(fn (a b) (+ a b)) 1"), Contains("This functions expects 2 arguments"));
     CHECK_THROWS_WITH(eval("(fn (a b) (+ a b)) 1 5 7"), Contains("This functions expects 2 arguments"));
